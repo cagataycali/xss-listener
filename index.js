c044d075b30fff850db4a53b40636ffa783ccfb7
willyb321 <wbwilliam7@gmail.com>
2017-05-15 09:28:35 +1000

    oops forgot to add to else

--- index.js
+++ index.js
@@ -20,10 +20,11 @@ bot.onText(/\/delete (.+)/, (msg, match) => {
     db.remove({}, { multi: true }, function (err, numRemoved) {
       bot.sendMessage(chatId, numRemoved);
     });
+  } else {
+    db.remove({ _id: resp }, {}, (err, numRemoved) => {
+      bot.sendMessage(chatId, numRemoved);
+    });
   }
-  db.remove({ _id: resp }, {}, (err, numRemoved) => {
-    bot.sendMessage(chatId, numRemoved);
-  });
 });
 
 // List gained rows ..
