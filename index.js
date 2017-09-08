67d30b69e2e8b7b4871991064c3cb914eaf121b8
willyb321 <wbwilliam7@gmail.com>
2017-09-08 11:30:18 +1000

    WIP: slack integration hopefully working
    now interaction is possible.

--- index.js
+++ index.js
@@ -2,34 +2,55 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const Datastore = require('nedb');
-const db = new Datastore({filename: 'out.db', autoload: true});
+const db = new Datastore({
+  filename: 'out.db',
+  autoload: true
+});
 const bodyParser = require('body-parser');
 const Telegram = require('node-telegram-bot-api');
 const SlackIncomingWebhook = require('@slack/client').IncomingWebhook;
+const RtmClient = require('@slack/client').RtmClient;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 let SlackWebhook;
 let chatId;
+let rtm;
 let TelegramBot;
 // Middleware usage ..
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.set('port', (process.env.PORT || 5000));
-if (process.env.SLACK_HOOK_URL) {
+if (process.env.SLACK_HOOK_URL && process.env.SLACK_RTM_TOKEN) {
   SlackWebhook = new SlackIncomingWebhook(process.env.SLACK_HOOK_URL || '');
+  const bot_token = process.env.SLACK_RTM_TOKEN || '';
+
+  rtm = new RtmClient(bot_token);
+
+  rtm.on(RTM_EVENTS.MESSAGE, getFromSlack);
+
+  rtm.start();
 }
 if (process.env.TELEGRAM_TOKEN && process.env.TELEGRAM_USER_ID) {
   chatId = process.env.TELEGRAM_USER_ID;
-  TelegramBot = new Telegram(process.env.TELEGRAM_TOKEN, {polling: true});
+  TelegramBot = new Telegram(process.env.TELEGRAM_TOKEN, {
+    polling: true
+  });
 }
 
 // Delete row ..
 TelegramBot.onText(/\/delete (.+)/, (msg, match) => {
   const resp = match[1]; // TODO @cagatay check the id is string, prevent nosql object injection.
   if (msg.text.endsWith('all')) {
-    db.remove({}, {multi: true}, function (err, numRemoved) {
+    db.remove({}, {
+      multi: true
+    }, function (err, numRemoved) {
       TelegramBot.sendMessage(chatId, `Removed ${numRemoved} entries from the db`);
     });
   } else {
-    db.remove({_id: resp}, {}, (err, numRemoved) => {
+    db.remove({
+      _id: resp
+    }, {}, (err, numRemoved) => {
       TelegramBot.sendMessage(chatId, `Removed ${numRemoved} entry from the db`);
     });
   }
@@ -38,7 +59,9 @@ TelegramBot.onText(/\/delete (.+)/, (msg, match) => {
 // List gained rows ..
 TelegramBot.onText(/\/list/, (msg) => {
   db.find({}, {}, (err, docs) => {
-    TelegramBot.sendMessage(chatId, '  🤙 here is your awesome xss request\'s list.. 👻 ``` \n' + JSON.stringify(docs, null, '\t') + ' \n ``` If you want to delete one of them, /delete [_id || all] 👍🏿 ', {parse_mode: 'Markdown'}).catch(err => {
+    TelegramBot.sendMessage(chatId, '  🤙 here is your awesome xss request\'s list.. 👻 ``` \n' + JSON.stringify(docs, null, '\t') + ' \n ``` If you want to delete one of them, /delete [_id || all] 👍🏿 ', {
+      parse_mode: 'Markdown'
+    }).catch(err => {
       if (err.code === 'ETELEGRAM' && err.response.body.description === 'Bad Request: message is too long') {
         TelegramBot.sendMessage(chatId, 'Too many docs for one message, sending them one by one.');
         for (let i = 0; i < docs.length; i++) {
@@ -53,6 +76,43 @@ TelegramBot.onText(/\/list/, (msg) => {
 TelegramBot.on('message', (msg) => {
   TelegramBot.sendMessage(chatId, `I'm awake! Your chat ID: ${chatId}, don't forget add in your code 🕷`);
 });
+
+// Fires on new message in slack.
+function getFromSlack(message) {
+  if (message.subtype !== 'bot_message') {
+    if (message.text.match(/\/delete (.+)/)) {
+      if (message.text.endsWith('all')) {
+        db.remove({}, {
+          multi: true
+        }, function (err, numRemoved) {
+          SlackWebhook.send(`Removed ${numRemoved} entries from the db`, );
+        });
+      } else {
+        db.remove({
+          _id: message.text.split(' ')[1].trim()
+        }, {}, (err, numRemoved) => {
+          SlackWebhook.send(`Removed ${numRemoved} entry from the db`, (err, header, statusCode, body) => {
+            if (err) {
+              console.log('Error:', err);
+            } else {
+              console.log('Received', statusCode, 'from Slack');
+            }
+          });
+        })
+      }
+    } else if (message.text.match(/\/list/)) {
+      db.find({}, {}, (err, docs) => {
+        SlackWebhook.send('  🤙 here is your awesome xss request\'s list.. 👻 ``` \n' + JSON.stringify(docs, null, '\t') + ' \n ``` If you want to delete one of them, /delete [_id || all] 👍🏿 ', (err, header, statusCode, body) => {
+          if (err) {
+            console.log('Error:', err);
+          } else {
+            console.log('Received', statusCode, 'from Slack');
+          }
+        });
+      })
+    }
+  }
+}
 
 // Send message to slack with data.
 function sendToSlack(doc) {
@@ -78,7 +138,9 @@ app.all('/:path*?', (req, res, next) => {
     db.insert(doc, (err, newDoc) => {
       if (newDoc && !err) {
         if (TelegramBot) {
-          TelegramBot.sendMessage(chatId, ' 🤘🏿 new baby on board! 👻 ``` \n' + JSON.stringify(doc, null, '\t') + ' \n ```', {parse_mode: 'Markdown'});
+          TelegramBot.sendMessage(chatId, ' 🤘🏿 new baby on board! 👻 ``` \n' + JSON.stringify(doc, null, '\t') + ' \n ```', {
+            parse_mode: 'Markdown'
+          });
         }
         if (SlackWebhook) {
           sendToSlack(doc);
@@ -101,11 +163,18 @@ app.get('/list', (req, res) => {
 // HTTP Delete endpoint ..
 app.get('/delete/:id?', (req, res) => {
   if (req.params.id && req.params.id.length > 0 && typeof req.params.id === 'string') {
-    db.remove({_id: req.params.id}, {}, (err, numRemoved) => {
-      res.json({err, numRemoved});
+    db.remove({
+      _id: req.params.id
+    }, {}, (err, numRemoved) => {
+      res.json({
+        err,
+        numRemoved
+      });
     });
   } else {
-    res.json({response: 'Please send _id with /delete/123'});
+    res.json({
+      response: 'Please send _id with /delete/123'
+    });
   }
 });
 
